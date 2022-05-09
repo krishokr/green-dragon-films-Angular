384014df4cc73735dd9832d98b648c0181a7377f
Kris Hokr <khokr@ucdavis.edu>
2022-05-09 13:48:17 -0700

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -25,96 +25,69 @@ export class UserRegistrationService {
   userLogin(userDetails: any): Observable<any> {
     console.log(userDetails);
     return this.http.post(apiUrl + 'login', userDetails).pipe( 
-      map(response => {
-        //need to store token??
-          let data = response.data;
-          localStorage.setItem('token', data.token);
-          localStorage.setItem('user', data.user);
-          return console.log(data);
-        }
-      )
-
+      map(res => this.extractResponseData)
     )
   }
 
   getMovie(movieTitle: string): Observable<any> {
-
-    const accessToken = localStorage.getItem('token');
-
-    if (accessToken) {
+      let accessToken = localStorage.getItem('token');
       return this.http.get(apiUrl + 'movies' + movieTitle, {
         headers: {Authorization: `Bearer ${accessToken}`}
       }).pipe(map(res => this.extractResponseData))
-    }
+    
   }
 
   getDirector(directorName: string): Observable<any> {
     const accessToken = localStorage.getItem('token');
-    if (accessToken) {
-      return this.http.get(apiUrl + 'directors/' + directorName, {
-        headers: {Authorization: `Bearer ${accessToken}`}
-      }).pipe(map(res => this.extractResponseData))
-    }
+    return this.http.get(apiUrl + 'directors/' + directorName, {
+      headers: {Authorization: `Bearer ${accessToken}`}
+    }).pipe(map(res => this.extractResponseData))
+    
   }
 
   getGenre(genreName: string): Observable<any> {
     const accessToken = localStorage.getItem('token');
-    if (accessToken) {
-      return this.http.get(apiUrl + 'genres/' + genreName, {
-        headers: {Authorization: `Bearer ${accessToken}`}
-      }).pipe(map(res => this.extractResponseData))
-    }
+    return this.http.get(apiUrl + 'genres/' + genreName, {
+      headers: {Authorization: `Bearer ${accessToken}`}
+    }).pipe(map(res => this.extractResponseData))
   }
 
   getUser(username: string): Observable<any> {
     const accessToken = localStorage.getItem("token");
-    if (accessToken) {
-      return this.http.get(apiUrl + 'users/' + username, {
-        headers: {Authorization: `Bearer ${accessToken}`}
-      }).pipe(map(res => this.extractResponseData))
-    }
+    return this.http.get(apiUrl + 'users/' + username, {
+      headers: {Authorization: `Bearer ${accessToken}`}
+    }).pipe(map(res => this.extractResponseData))
   }
 
   //get favorite movies from user info (use info from getUser)
-  public getFavoriteMovies(): Observable<any> {
-    const favoriteMovies = this.getUser().FavoriteMovies;
-    return this.extractResponseData(favoriteMovies);
-  }
 
   public addFavoriteMovie(username: string, movieID: string): Observable<any> {
     const accessToken = localStorage.getItem('token');
-    if (accessToken) {
-      return this.http.post(apiUrl + 'users/' + username + '/movies/' + movieID, {
-        headers: {Authorization: `Bearer ${accessToken}`}
-      }).pipe(map(res => console.log(res)))
-    }
+    return this.http.post(apiUrl + 'users/' + username + '/movies/' + movieID, {
+      headers: {Authorization: `Bearer ${accessToken}`}
+    }).pipe(map(res => console.log(res)))
   }
 
   public deleteFavoriteMovie(username: string, movieID: string): Observable<any> {
     const accessToken = localStorage.getItem("token");
-    if (accessToken) {
-      return this.http.delete(apiUrl + 'users/' + username + '/movies/' + movieID, {
-        headers: {Authorization: `Bearer ${accessToken}`}
-      }).pipe(map(res => console.log(res)))
-    }
+    return this.http.delete(apiUrl + 'users/' + username + '/movies/' + movieID, {
+      headers: {Authorization: `Bearer ${accessToken}`}
+    }).pipe(map(res => console.log(res)))
   }
 
   public updateUser(username: string, userDetails: any): Observable<any> {
     const accessToken = localStorage.getItem('token');
-    if (accessToken) {
-      return this.http.put(apiUrl + 'users/' + username, userDetails, {
-        headers: {Authorization: `Bearer ${accessToken}`}
-      }).pipe(map(res => console.log(res)))
-    }
+    return this.http.put(apiUrl + 'users/' + username, userDetails, {
+      headers: {Authorization: `Bearer ${accessToken}`}
+    }).pipe(map(res => console.log(res)))
+    
   }
 
   public deleteUser(username: string): Observable<any> {
     const accessToken = localStorage.getItem('token');
-    if (accessToken) {
-      return this.http.put(apiUrl + 'users/' + username, {
-        headers: {Authorization: `Bearer ${accessToken}`}
-      }).pipe(map(res => console.log(res)))
-    }
+    return this.http.put(apiUrl + 'users/' + username, {
+      headers: {Authorization: `Bearer ${accessToken}`}
+    }).pipe(map(res => console.log(res)))
   }
 
   private handleError(error: HttpErrorResponse): any {
@@ -144,20 +117,9 @@ export class UserRegistrationService {
     )
   }
 
-  
-
   private extractResponseData(res: Response): any {
     const body = res;
     return body || {}
   }
 
-
 }
-
-
-// export class FetchApiDataService {
-//   constructor() { }
-
-  
-
-// }
