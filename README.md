bc6b7b61fafd867dae7286a904f46ec20687ebfd
Kris Hokr <khokr@ucdavis.edu>
2022-05-18 14:18:11 -1000

    updated readme

--- README.md
+++ README.md
@@ -1,37 +1,58 @@
-# GreenDragonFilmsAngular
+# Green Dragon Films Movie App - Angular
+### Last Updated: May 18, 2022
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.5.
+<img src="./assets/demo1.png" width="300">
+<p left="float">
+    <img src="./assets/demo2.png" width="300">
+    <img src="./assets/demo3.png" width="300">
+</p>
 
-Overview
+## Overview
 
 Green Dragon Films - Angular is a frontend application that uses Angular and TypeScript to allow users to login and fan over their favorite action and adventure movies. They can view details about a movie's genre, director, and description, as well as store it in their account as a favorite.
 
-Purpose & Context
+## Purpose & Context
 
-Green Dragon Films - Angular is a personal project I built to test my Angular and TypeScript skills I learned as part of a web development course called Career Foundry. It's the Angular version of the frontend application that integrates an API I built using Express and Node. I also built a React version, which can be seen on my portfolio site.
-
-Objective
-
-The aim of this project was to build an Angular application I can add to my professional portfolio. The problem I wanted to solve was to build a frontend for my Movies API using Angular and TypeScript.
+Green Dragon Films - Angular is a personal project I built to test my Angular and TypeScript skills I learned as part of a web development course called Career Foundry. It's the Angular version of the frontend application that integrates an API I built using Express and Node. I also built a React version, which can be seen at [https://greendragonfilms.netlify.app/](https://greendragonfilms.netlify.app/).
 
 
-Approach
+## Approach
 
-Server-Side
+### Server-Side
+I built a Movies RESTful API that uses MongoDB to store JSON files about movies and users. You can view the documentation at [https://greendragonflix.herokuapp.com/](https://greendragonflix.herokuapp.com/).
 
-My Movies API uses MongoDB to store information about movies and users. The API stores data using JSON format and is accessed through https://greendragonflix.herokuapp.com/.
-
-Client-Side
-
+### Client-Side
 The client-side of the application is the main focus of the project using Angular and TypeScript. Initially, users are prompted to either login or register using their username and password. Once the user is validated, they are redirected to the main page with displays movie cards that allow the user to view clickable information on their favorite movies such as the genre, director, and description. On clicking the card, the user is directed to a single movie page where they can view more information about their favorite movies. Users can like a movie, which will add it to their favorites for them to view on the favorite movies page. From the Account button, users are able to update their personal information such as their username, password, and email.
 
-Credits Lead Developer and Designer: Kristofer Hokr
 
-                                -------------     
+## Credits Lead Developer and Designer: Kristofer Hokr
 
-Development Environment Requirement and Libraries
+## Dependencies
+- Angular v13.3.5 or higher
+- TypeScript v4.6.2 or higher
+- Material UI v13.3 or higher
 
-Angular v13.3.5 or higher
-TypeScript v4.6.2 or higher
-Material UI v13.3 or higher
+## Getting Started
+
+You can also view this project at (https://green-dragon-films-angular.netlify.app/)[https://green-dragon-films-angular.netlify.app/].
+
+Clone this repository
+
+```git clone https://github.com/krishokr/green-dragon-films-Angular.git```
+
+Go to project's root directory
+
+```cd green-dragon-Angular```
+
+Install dependencies
+
+```npm install```
+
+
+Run the project using angular
+
+```ng serve```
+
+
+
 
--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -11,13 +11,26 @@ const apiUrl = 'https://greendragonflix.herokuapp.com/';
 })
 
 export class UserRegistrationService {
-
+  /**
+   * @param http Constructor imports HttpClient
+   */
   constructor(private http: HttpClient) {}
 
+  /**
+   * Function for registering a new user.
+   * @param userDetails An object that contains the new user's username, email, password, and birthday.
+   * @returns The http response to the user registration request from the API.
+   */
   userRegistration(userDetails: any): Observable<any> {
     console.log(userDetails);
     return this.http.post(apiUrl + 'users', userDetails).pipe(catchError(this.handleError));
   }
+
+  /**
+   * Function that allows users to login.
+   * @param userDetails An object that contains the user's username and password.
+   * @returns If the user's username and password were correct, the function will return the token and the username for local storage.
+   */
 
   userLogin(userDetails: any): Observable<any> {
     console.log(userDetails);
@@ -27,6 +40,12 @@ export class UserRegistrationService {
     
   }
 
+  /**
+   * Function that gets a specific movie from the API.
+   * @param movieTitle A movie's title.
+   * @returns An object with the specific movie's data including its title, director, genre, and description.
+   */
+
   getMovie(movieTitle: string | null): Observable<any> {
       let accessToken = localStorage.getItem('token');
       return this.http.get(apiUrl + 'movies/' + movieTitle, {
@@ -34,6 +53,12 @@ export class UserRegistrationService {
       }).pipe(catchError(this.handleError))
     
   }
+
+  /**
+   * Function that gets information about a specific director.
+   * @param directorName A director's name as a string.
+   * @returns An object with information about the director, including their name, birth year, and biography.
+   */
 
   getDirector(directorName: string): Observable<any> {
     const accessToken = localStorage.getItem('token');
@@ -43,12 +68,23 @@ export class UserRegistrationService {
     
   }
 
+  /**
+   * Function that gets information about a specific genre.
+   * @param genreName A specific genre's name as a string.
+   * @returns An object with information about the genre including the genre's name and description.
+   */
+
   getGenre(genreName: string): Observable<any> {
     const accessToken = localStorage.getItem('token');
     return this.http.get(apiUrl + 'genres/' + genreName, {
       headers: {Authorization: `Bearer ${accessToken}`}
     }).pipe(map(res => this.extractResponseData))
   }
+
+  /**
+   * Function that gets information about the current user.
+   * @returns An object with information about a user including their username, email, birthday, hashed password, and favorite movies.
+   */
 
   getUser(): Observable<any> {
     const accessToken = localStorage.getItem("token");
@@ -58,7 +94,12 @@ export class UserRegistrationService {
     })
   }
 
-  //get favorite movies from user info (use info from getUser)
+  
+  /**
+   * Function that adds a new favorite movie to the user's favorite movies.
+   * @param movieID The movie ID of the movie to add as a string.
+   * @returns A response status ensuring the movie has been added.
+   */
 
   addFavoriteMovie(movieID: string): Observable<any> {
     const accessToken = localStorage.getItem('token');
@@ -69,6 +110,11 @@ export class UserRegistrationService {
     }).pipe(catchError(this.handleError))
   }
 
+  /**
+   * Function that deletes a favorite movie from the user's favorite movies.
+   * @param movieID The movie ID of the movie to delete as a string.
+   * @returns A response status ensuring the movie has been deleted.
+   */
   deleteFavoriteMovie(movieID: string): Observable<any> {
     const accessToken = localStorage.getItem("token");
     const username = localStorage.getItem('user');
@@ -76,6 +122,12 @@ export class UserRegistrationService {
       headers: {Authorization: `Bearer ${accessToken}`}
     }).pipe(catchError(this.handleError))
   }
+
+  /**
+   * Function that allows a user to update their information.
+   * @param userDetails An object with the user's updated and old information including the username, password, email, birthday, and favorite movies.
+   * @returns A response status ensuring the user's information has been updated.
+   */
 
   updateUser(userDetails: any): Observable<any> {
     const accessToken = localStorage.getItem('token');
@@ -86,12 +138,23 @@ export class UserRegistrationService {
     
   }
 
+  /**
+   * Function that allows the user to delete their profile.
+   * @param username The user's username as a string.
+   * @returns A response status indicating the success or failure of the deletion.
+   */
   deleteUser(username: string): Observable<any> {
     const accessToken = localStorage.getItem('token');
     return this.http.put(apiUrl + 'users/' + username, {
       headers: {Authorization: `Bearer ${accessToken}`}
     }).pipe(catchError(this.handleError))
   }
+
+  /**
+   * Error-handling function.
+   * @param error Http response error from any API call.
+   * @returns Specific error in console, as well as a string that warns the user the request did not go through.
+   */
 
   handleError(error: HttpErrorResponse): any {
     if (error.error instanceof ErrorEvent) {
@@ -105,6 +168,11 @@ export class UserRegistrationService {
     'Something bad happened; please try again later.');
   }
 
+  /**
+   * Function that gets all movies.
+   * @returns An array with all the movies in the database as objects.
+   */
+
   getAllMovies(): Observable<any> {
     const token = localStorage.getItem('token');
 
@@ -117,6 +185,11 @@ export class UserRegistrationService {
     })
   }
 
+  /**
+   * Helper function that allows service functions to format response data.
+   * @param res Response from any API call.
+   * @returns The response, or an empty object.
+   */
   private extractResponseData(res: Response): any {
     const body = res;
     return body || {}
